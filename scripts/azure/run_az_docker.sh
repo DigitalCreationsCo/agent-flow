b8977062cb7106c1e830322a057c88d249a50fe9
Bryant Mejia <bmejia220@gmail.com>
2025-02-02 02:15:30 -0800

    docker mount .docker

--- scripts/azure/run_az_docker.sh
+++ scripts/azure/run_az_docker.sh
@@ -4,6 +4,7 @@ docker build -f az.Dockerfile -t azure-docker-cli . || { echo "Docker build fail
 # Run the container with Docker socket mounted
 docker run -it \
   -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /Users/bryantmejia/.docker:/root/.docker \
   -v $HOME/.azure:/root/.azure \
   azure-docker-cli
 
